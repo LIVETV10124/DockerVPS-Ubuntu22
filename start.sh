bee3851071882f894ed90c0a2fc3256473c10baf
LIVETV10124 <salinadabvg@gmail.com>
2026-03-13 10:10:38 +0600

    Refactor start.sh to improve service startup order
    Updated start.sh to include Node.js backend and organized service starts.

--- start.sh
+++ start.sh
@@ -4,13 +4,15 @@ set -e
 # Start SSH
 service ssh start
 
-# Start filebrowser on port 8080 (default landing page)
+# Start services
 filebrowser -r / -p 8080 &
-
-# Start wetty (Web SSH) on port 10000
 wetty --port 10000 &
+sshx &
 
-# Start nginx (reverse-proxy everything to port 80)
+# Start Node.js backend for stats & control
+node /backend.js &
+
+# Start Nginx
 service nginx start
 
 # Keep container alive
