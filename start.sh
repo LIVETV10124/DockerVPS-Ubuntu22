3d791482172b9e8674ccec9f0b8cb34be1b80df8
LIVETV10124 <salinadabvg@gmail.com>
2026-03-13 10:03:10 +0600

    Refactor start.sh to improve service startup order
    Reorganized service starts and updated comments.

--- start.sh
+++ start.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 set -e
 
-# ----------------------------
 # Start SSH
-# ----------------------------
 service ssh start
 
-# ----------------------------
-# Start Nginx
-# ----------------------------
-service nginx start
-
-# ----------------------------
-# Start Filebrowser (Web File Manager)
-# ----------------------------
+# Start filebrowser on port 8080 (default landing page)
 filebrowser -r / -p 8080 &
 
-# ----------------------------
-# Start ttyd (Web SSH terminal)
-# ----------------------------
-ttyd -p 10000 bash
+# Start wetty (Web SSH) on port 10000
+wetty --port 10000 &
+
+# Start nginx (reverse-proxy everything to port 80)
+service nginx start
+
+# Keep container alive
+tail -f /dev/null
